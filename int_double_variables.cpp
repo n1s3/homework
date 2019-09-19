428cb2300a7d6c15f2549441f09839eeb8c22e6e
n1s3 <55400664+n1s3@users.noreply.github.com>
2019-09-19 18:59:32 +0300

    Add files via upload

---
+++ int_double_variables.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Переменные int и double. \n";
+
+	
+	int a = 3, b = 3.14;
+	double c = 3, d = 3.14;
+	cout << "Целочисленные переменные:\n" << "1. " << a << "\n" << "2. " << b << "\n";
+	cout << "Дробные переменные:\n" << "1. " << c << "\n" << "2. " << d << "\n\n";
+}
