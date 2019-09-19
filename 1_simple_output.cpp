2b24ff22f4dd5f790c87aecc2725d7a2e53ea4ee
n1s3 <55400664+n1s3@users.noreply.github.com>
2019-09-19 18:36:50 +0300

    Add files via upload

---
+++ 1_simple_output.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	std::cout << "Простой вывод. \n";
+
+	setlocale(LC_ALL, "Russian");
+	int a;
+	a = 2 * 2;
+	a = a + 2;
+	std::cout << "Результат вычисления выражения 2+2*2 = " << a << endl;	
+}
+
