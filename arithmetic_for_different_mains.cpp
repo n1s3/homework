500f04479ab36560d8d5cea0656785dcdd6f16c3
n1s3 <55400664+n1s3@users.noreply.github.com>
2019-09-19 19:02:50 +0300

    Add files via upload

---
+++ arithmetic_for_different_mains.cpp
@@ -0,0 +1,41 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a, b;
+	double c, d; 
+	std::cout << " Арифметика для разных типов. \n";
+
+	std::cout << "Введите две целые переменные: \n";
+	std::cin >> a >> b;
+	std::cout << "\n" << a << "+" << b << "=" << a + b;
+	std::cout << "\n" << a << "-" << b << "=" << a - b;
+	std::cout << "\n" << a << "*" << b << "=" << a * b;
+	std::cout << "\n" << a << "/" << b << "=" << a / b;
+
+	std::cout << "\n" << "Введите две  дробные переменные: \n";
+	std::cin >> c >> d;
+	std::cout << "\n" << c << "+" << d << "=" << c + d;
+	std::cout << "\n" << c << "-" << d << "=" << c - d;
+	std::cout << "\n" << c << "*" << d << "=" << c * d;
+	std::cout << "\n" << c << "/" << d << "=" << c / d;
+
+	std::cout << "\n" << "Введите сначала целую переменную, а потом дробную: \n";
+	std::cin >> a >> c;
+	std::cout << "\n" << a << "+" << c << "=" << a + c;
+	std::cout << "\n" << a << "-" << c << "=" << a - c;
+	std::cout << "\n" << a << "*" << c << "=" << a * c;
+	std::cout << "\n" << a << "/" << c << "=" << a / c;
+
+	std::cout << "\n" << "Введите сначала дробную переменную, а потом целую: \n";
+	std::cin >> d >> b;
+	std::cout << "\n" << d << "+" << b << "=" << d + b;
+	std::cout << "\n" << d << "-" << b << "=" << d - b;
+	std::cout << "\n" << d << "*" << b << "=" << d * b;
+	std::cout << "\n" << d << "/" << b << "=" << d / b << "\n\n";
+}
+
---
+++ exchange_of_values.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Обмен значениями. \n";
+
+	int a, b, c;
+	std::cout << "\nВведите a: \n";
+	std::cin >> a;
+	std::cout << "\nВведите b: \n";
+	std::cin >> b;
+	c = a;
+	a = b;
+	b = c;
+	std::cout <<" \n поменяем местами переменные с помощью дополнительной переменной \n" << "a = " << a << "\nb = " << b;
+
+	std::cout << "\nВведите a: \n";
+	std::cin >> a;
+	std::cout << "\nВведите b: \n";
+	std::cin >> b;
+	std::swap(a, b);
+	std::cout << " \n поменяем местами переменные без дополнительной переменной \n" << "a = " << a << "\nb = " << b;
+}
+
