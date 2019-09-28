7ef8eb1d5754bf09374777b96c4dd3a8db54a0ca
n1s3 <55400664+n1s3@users.noreply.github.com>
2019-09-28 20:25:53 +0300

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a, n;
+	bool prostoe = true;
+	cout << "введите число: ";
+	cin >> a;
+	for (n = 2; n <= (sqrt(abs(a))); n++)
+	{
+		if (a % n == 0)
+		{
+			prostoe = false;
+			break;
+		}
+	}
+	if (prostoe)
+		cout << "простое" << endl;
+	else
+		cout << "составное " << endl;
+}
