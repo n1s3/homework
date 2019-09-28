36435a4cff38ccd37895f6e6418306c6433e771e
n1s3 <55400664+n1s3@users.noreply.github.com>
2019-09-28 19:57:47 +0300

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	float a, b, c, D, x, x1, x2;
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Дано уравнение вида: a * x * x + b * x + c = 0\n";
+	cout << "Введите некоторые данные:\n";
+	cout << "a = ";
+	cin >> a;
+	cout << "\nb = ";
+	cin >> b;
+	cout << "\nc = ";
+	cin >> c;
+	
+	if (a == 0)
+	{
+		x = -c / b;
+		cout << "\nx = " << x << endl;
+	}
+	else
+	{
+		D = b * b - 4 * a * c;
+		if (D > 0)
+		{
+			x1 = -b + sqrt(D) / (2 * a);
+			x2 = -b - sqrt(D) / (2 * a);
+			cout << "\nx1 = " << x1 << "\nx2 = " << x2 << endl;
+		}
+		if (D < 0)
+		{
+			cout << "корней нет";
+		}
+		if (D == 0)
+		{
+			x = -b / (2 * a);
+			cout << "\nx = " << x << endl;
+		}
+	}
+}
+
---
+++ 8.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, d;
+	char c;
+	cin >> a >> c >> b;
+	if (c == '+')
+	{
+		cout << a + b;
+	}
+	if (c == '-')
+	{
+		cout << a - b;
+	}
+	if (c == '*')
+	{
+		cout << a * b;
+	}
+	if (c == '/')
+	{
+		if (b == 0)
+		{
+			cout << "деление на ноль";
+		}
+		else
+		cout << a / b;
+
+	}
+	
+}
