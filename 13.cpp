a2fc12c547266c761207ff1c6aacaf786ab2d254
n1s3 <55400664+n1s3@users.noreply.github.com>
2019-11-15 09:05:14 +0300

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,15 @@
+﻿#include <math.h>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	int n,result, i = 0;
+	cin >> n;
+	while (n > 0) {
+		n = n - pow(2, i);
+//		cout << pow(2, i) << " " ;
+		i++;
+	};
+	cout << endl << "Count of numbers: " << i;
+}
---
+++ 15.cpp
@@ -0,0 +1,42 @@
+﻿#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int sort(int* arr, int size) {
+	int temp;
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	return *arr;
+}
+
+int main()
+{
+	int n;
+	cout << "Enter n:\n";
+	cin >> n;
+	int* arr = new int[n]; //память
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, n);
+	int counter = 0;
+	for (int i = 0; i < n; i++) {
+		if (counter < 5) {
+			counter++;
+		};
+		for (int j = 0; j < counter; j++) {
+			cout << arr[counter-j-1] << " ";
+		};
+		cout << endl;
+	};
+	delete[] arr; // очистка памяти
+}
+
